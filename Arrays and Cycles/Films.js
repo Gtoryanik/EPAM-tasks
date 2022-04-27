de54f47b23f7e51068aaf150923fd3f830049fa8
Gtoryanik <102662879+Gtoryanik@users.noreply.github.com>
2022-04-27 11:11:08 +0400

    Delete Films.js

--- Arrays and Cycles/Films.js
+++
@@ -1,5 +0,0 @@
-let films = ['человек-паук', 'человек-молекула', 'человек дождя', 'себрский фильм'];
-films.forEach(function(item, i, films) {
-    alert( i + ": " + item + " (массив:" + films + ")" );
-}
-)
