7322824d9f8e67885ed4d8dcc6eaa72870e3e2c5
Gtoryanik <102662879+Gtoryanik@users.noreply.github.com>
2022-04-27 10:10:07 +0400

    Update IndexOfArrayElement.js

--- Arrays and Cycles/IndexOfArrayElement.js
+++ Arrays and Cycles/IndexOfArrayElement.js
@@ -1,6 +1,6 @@
-let numbers = [1,2,10,26,18,56]
+let numbers = [1,2,10,26,18,56];
 let IndexElement = (array, number) => {
-let getIndex = array.findIndex ((element) => element == number)
-    return getIndex
+let getIndex = array.findIndex ((element) => element == number);
+    return getIndex;
 }
-IndexElement (numbers, 18)
+IndexElement (numbers, 18);
