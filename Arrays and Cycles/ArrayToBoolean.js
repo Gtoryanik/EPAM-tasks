9cdfde5b10d0a084604544934d3e557f954e3d2a
Gtoryanik <102662879+Gtoryanik@users.noreply.github.com>
2022-04-27 09:58:03 +0400

    Update ArrayToBoolean.js

--- Arrays and Cycles/ArrayToBoolean.js
+++ Arrays and Cycles/ArrayToBoolean.js
@@ -1,6 +1,6 @@
 let numbers = [0,1,2,3,4,5,6]
-for (let i = 0; i < 7; i++) {
-console.log (Boolean (numbers[i]))
+  for (let i = 0; i < 7; i++) {
+    console.log (Boolean (numbers[i]))
 }
 
 console.log (numbers.reverse())
