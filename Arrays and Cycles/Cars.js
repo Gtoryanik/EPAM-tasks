4055c11f164c70c90dc3385f51e382ac58f70b29
Gtoryanik <102662879+Gtoryanik@users.noreply.github.com>
2022-04-27 10:08:09 +0400

    Update Cars.js

--- Arrays and Cycles/Cars.js
+++ Arrays and Cycles/Cars.js
@@ -1,5 +1,5 @@
-let cars = ['жигули', 'ауди', 'тесла', 'форд']
-let carsString = JSON.stringify(cars)
-console.log (carsString)
-let carsArray = JSON.parse(carsString)
-console.log (carsArray)
+let cars = ['жигули', 'ауди', 'тесла', 'форд'];
+let carsString = JSON.stringify(cars);
+console.log (carsString);
+let carsArray = JSON.parse(carsString);
+console.log (carsArray);
