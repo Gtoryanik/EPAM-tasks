f83c813bfdac7ce40c2c167ce25bfe0ce1b78e91
Gtoryanik <102662879+Gtoryanik@users.noreply.github.com>
2022-04-27 10:10:56 +0400

    Update ConditionalStatementsAndDataTypes.js

--- Conditional Statements and Data Types/ConditionalStatementsAndDataTypes.js
+++ Conditional Statements and Data Types/ConditionalStatementsAndDataTypes.js
@@ -1,14 +1,14 @@
-let a = '1'
-let b = false
-let c = 2
-console.log (a+b)
-console.log (a+c)
-console.log (c+b)
+let a = '1';
+let b = false;
+let c = 2;
+console.log (a+b);
+console.log (a+c);
+console.log (c+b);
 
-console.log (a*b)
-console.log (a*c)
-console.log (c*b)
+console.log (a*b);
+console.log (a*c);
+console.log (c*b);
 
-console.log (a/b)
-console.log (a/c)
-console.log (c/b)
+console.log (a/b);
+console.log (a/c);
+console.log (c/b);
