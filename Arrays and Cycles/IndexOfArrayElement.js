31477e87a68fd39eeafd40d13a9306169fbcfda0
Gtoryanik <102662879+Gtoryanik@users.noreply.github.com>
2022-04-27 11:17:08 +0400

    Delete IndexOfArrayElement.js

--- Arrays and Cycles/IndexOfArrayElement.js
+++
@@ -1,6 +0,0 @@
-let numbers = [1,2,10,26,18,56];
-let IndexElement = (array, number) => {
-let getIndex = array.findIndex ((element) => element == number);
-    return getIndex;
-}
-IndexElement (numbers, 18);
