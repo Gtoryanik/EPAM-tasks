82079ba6a08a36fe1d36e31a16d960dc3a434f44
Gtoryanik <102662879+Gtoryanik@users.noreply.github.com>
2022-04-27 10:14:22 +0400

    Update RegularExpressions.js

--- Prototype and Inheritance, Classes, Constructor/RegularExpressions.js
+++ Prototype and Inheritance, Classes, Constructor/RegularExpressions.js
@@ -1,7 +1,7 @@
-let str = '2+3 223 2223'
-let result = str.match(/(2\+3)/g)//
-alert(result)
+let str = '2+3 223 2223';
+let result = str.match(/(2\+3)/g)//;
+alert(result);
 
 let string = 'ahb acb aeb aeeb adcb axeb';
 let stringResult = string.match(/a.b/g);
-console.log(stringResult)
+console.log(stringResult);
