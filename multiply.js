e93eb050e5f460057d47f9dc427378a66fb5b6cb
Gtoryanik <102662879+Gtoryanik@users.noreply.github.com>
2022-05-17 15:58:36 +0400

    Create multiply.js

---
+++ multiply.js
@@ -0,0 +1,32 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable require-jsdoc */
+/* eslint-disable space-before-blocks */
+/**
+ *
+ * A method calculating a * b
+ * @class NumbersMultiply
+ */
+class NumbersMultiply {
+  constructor(){
+  }
+  /**
+       *
+       *
+       * @param {Number} a is a first number
+       * @param {Number} b is a second number
+       * @return {Number} return a+b if a && b is a number
+       * @memberof addNumbers
+       */
+  calc(a, b) {
+    const typeOfVariableA = typeof a;
+    const typeOfVariableB = typeof b;
+    if (typeOfVariableA !== 'number' ||
+              typeOfVariableB !== 'number') {
+      throw new Error('It is not a type of number');
+    } else {
+      return a * b;
+    };
+  };
+}
+module.exports = NumbersMultiply;
+
