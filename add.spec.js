40a7c90cba659528541c121defae756a2120bca3
Gtoryanik <102662879+Gtoryanik@users.noreply.github.com>
2022-05-17 15:58:14 +0400

    Create add.spec.js

---
+++ add.spec.js
@@ -0,0 +1,17 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+const NumbersAdd = require('../../app/Add');
+const {expect} = require('chai');
+
+describe('is number is a number positive scenarios', function() {
+  let validator;
+  beforeEach(function() {
+    validator = new NumbersAdd;
+  });
+  afterEach(function() {
+    validator = null;
+  });
+  it('should return a + b if a & b is numbers', function() {
+    expect(validator.calc(4, 2)).to.be.equal(6);
+  });
+});
